e00bc948f7607c21fee67da57edf78d611ef4838
RijanPrajapati1 <122766161+RijanPrajapati1@users.noreply.github.com>
2024-11-26 08:16:14 +0545

    little update

--- controller/customer_controller.js
+++ controller/customer_controller.js
@@ -7,18 +7,19 @@ const findAll = async (req, res) => {
         const customers = await customer.find();
         res.status(200).json(customers);
     } catch (e) {
-        res.json(e)
+        res.status(500).json(e)
     }
 }
 
 
 const save = async (req, res) => {
     try {
-        const customers = new customer(req.body);
+        const{body}=req
+        const customers = new customer(body);
         await customers.save()
-        res.status(201).json(customers)
+        res.status(201).json(customers)   
     } catch (e) {
-        res.json(e)
+        res.status(500).json(e)
     }
 }
 
@@ -27,7 +28,7 @@ const findById=async(req,res) => {
         const customers=await customer.findById(req.params.id);
         res.status(200).json(customers)
     }catch(e){
-        res.json(e)
+        res.status(500).json(e)
     }
 }
 
@@ -37,7 +38,7 @@ const deleteById=async(req,res) => {
         const customers=await customer.findByIdAndDelete(req.params.id);
         res.status(200).json("Data Deleted")
     }catch(e){
-        res.json(e)
+        res.status(500).json(e)
     }
 }
 
@@ -46,7 +47,7 @@ const update=async(req,res) => {
         const customers=await customer.findByIdAndUpdate(req.params.id,req.body,{new:true});
         res.status(202).json(customers)
     }catch(e){
-        res.json(e)
+        res.status(500).json(e)
     }
 }
 
